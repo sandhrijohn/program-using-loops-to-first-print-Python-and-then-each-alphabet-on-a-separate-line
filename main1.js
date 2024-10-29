ad7069424342f0fbabe427d825d2006ffab0c258
SANDHRI JOHN ABHISHEK <asandhrijohn@gmail.com>
2024-10-29 13:54:40 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,8 @@
+const courses = ["Python"];
+for(let x = 0; x < courses.length; x++) {
+      console.log(courses[x]);
+      for(let y = 0; y < courses[x].length; y++) {
+            console.log(courses[x][y]);
+         }
+   }
+
